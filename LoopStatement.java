eb283a5447227baa94e09fee270c8876337ca3e4
Sameer shrinath <sameershrinath@gmail.com>
2024-10-22 11:11:26 +0530

    Bootcamp day 2

---
+++ LoopStatement.java
@@ -0,0 +1,12 @@
+public class LoopStatement {
+    public static void main(String[] args) {
+        int no = 5;
+        System.out.println("Loop starts");
+        
+        // Initialize 'i' to 0
+        for (int i = 0; i < no; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Loop ends");
+    }
+}
